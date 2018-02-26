156b89111d0cfb4d554402dae50654da986b17b6
156509674@qq.com <156509674@qq.com>
2018-02-26 16:11:04 +0800

    添加了第一个VUE.js测试项目资料

---
+++ zzyProject/server/db.js
@@ -0,0 +1,11 @@
+//db.js——用来添加 mysql 配置
+// 数据库连接配置
+module.exports = {
+    mysql: {
+        host: 'localhost',
+        user: 'root',
+        password: 'ZZY',
+        database: 'iis_db',
+        port: '3306'
+    }
+}
